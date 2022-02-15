crazyinsanejames/device_oneplus_sm8150-Just-Building

DeviceSettings: Follow A12 settings style & cleanup
Do a big cleanup everyewhere. Still a lot more to do.
Am too lazy to describe all I did. Self explanatory anyways. It was a big mess.

Adapting to A12 style partially taken from: https://github.com/TheScarastic/android_device_xiaomi_raphael/commit/e7db7fc560787440784b6c9787da2e1ab2695f59

Co-authored-by: TheScarastic <warabhishek@gmail.com>
Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

DeviceSettings/src/org/lineageos/device/DeviceSettings/ModeSwitch/DCIModeSwitch.java
@@ -17,12 +17,10 @@
 */
 package org.lineageos.device.DeviceSettings;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.SystemProperties;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceManager;
+
+import org.lineageos.device.DeviceSettings.Utils;
 
 public class DCIModeSwitch implements OnPreferenceChangeListener {
 
@@ -39,7 +37,7 @@ public class DCIModeSwitch implements OnPreferenceChangeListener {
         return Utils.fileWritable(getFile());
     }
 
-    public static boolean isCurrentlyEnabled(Context context) {
+    public static boolean isCurrentlyEnabled() {
         return Utils.getFileValueAsBoolean(getFile(), false);
     }
 

DeviceSettings/src/org/lineageos/device/DeviceSettings/ModeSwitch/DCModeSwitch.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2018 The OmniROM Project
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -17,13 +17,10 @@
 */
 package org.lineageos.device.DeviceSettings;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceManager;
 
-import org.lineageos.device.DeviceSettings.DeviceSettings;
+import org.lineageos.device.DeviceSettings.Utils;
 
 public class DCModeSwitch implements OnPreferenceChangeListener {
 
@@ -40,7 +37,7 @@ public class DCModeSwitch implements OnPreferenceChangeListener {
         return Utils.fileWritable(getFile());
     }
 
-    public static boolean isCurrentlyEnabled(Context context) {
+    public static boolean isCurrentlyEnabled() {
         return Utils.getFileValueAsBoolean(getFile(), false);
     }
 

DeviceSettings/src/org/lineageos/device/DeviceSettings/ModeSwitch/SRGBModeSwitch.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2018 The OmniROM Project
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -17,11 +17,10 @@
 */
 package org.lineageos.device.DeviceSettings;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceManager;
+
+import org.lineageos.device.DeviceSettings.Utils;
 
 public class SRGBModeSwitch implements OnPreferenceChangeListener {
 
@@ -38,7 +37,7 @@ public class SRGBModeSwitch implements OnPreferenceChangeListener {
         return Utils.fileWritable(getFile());
     }
 
-    public static boolean isCurrentlyEnabled(Context context) {
+    public static boolean isCurrentlyEnabled() {
         return Utils.getFileValueAsBoolean(getFile(), false);
     }
 

FabricioSC123/device_oneplus_cheeseburger

DeviceSettings/src/com/radiant/device/DeviceSettings/ModeSwitch/DCIModeSwitch.java
@@ -15,14 +15,12 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.radiant.device.DeviceSettings;
+package com.radiant.device.DeviceSettings.ModeSwitch;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.SystemProperties;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceManager;
+
+import com.radiant.device.DeviceSettings.Utils;
 
 public class DCIModeSwitch implements OnPreferenceChangeListener {
 
@@ -39,7 +37,7 @@ public class DCIModeSwitch implements OnPreferenceChangeListener {
         return Utils.fileWritable(getFile());
     }
 
-    public static boolean isCurrentlyEnabled(Context context) {
+    public static boolean isCurrentlyEnabled() {
         return Utils.getFileValueAsBoolean(getFile(), false);
     }
 

DeviceSettings/src/com/radiant/device/DeviceSettings/ModeSwitch/DCModeSwitch.java
@@ -15,15 +15,12 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.radiant.device.DeviceSettings;
+package com.radiant.device.DeviceSettings.ModeSwitch;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceManager;
 
-import com.radiant.device.DeviceSettings.DeviceSettings;
+import com.radiant.device.DeviceSettings.Utils;
 
 public class DCModeSwitch implements OnPreferenceChangeListener {
 
@@ -40,7 +37,7 @@ public class DCModeSwitch implements OnPreferenceChangeListener {
         return Utils.fileWritable(getFile());
     }
 
-    public static boolean isCurrentlyEnabled(Context context) {
+    public static boolean isCurrentlyEnabled() {
         return Utils.getFileValueAsBoolean(getFile(), false);
     }
 

DeviceSettings/src/com/radiant/device/DeviceSettings/ModeSwitch/SRGBModeSwitch.java
@@ -15,13 +15,12 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.radiant.device.DeviceSettings;
+package com.radiant.device.DeviceSettings.ModeSwitch;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceManager;
+
+import com.radiant.device.DeviceSettings.Utils;
 
 public class SRGBModeSwitch implements OnPreferenceChangeListener {
 
@@ -38,7 +37,7 @@ public class SRGBModeSwitch implements OnPreferenceChangeListener {
         return Utils.fileWritable(getFile());
     }
 
-    public static boolean isCurrentlyEnabled(Context context) {
+    public static boolean isCurrentlyEnabled() {
         return Utils.getFileValueAsBoolean(getFile(), false);
     }
 