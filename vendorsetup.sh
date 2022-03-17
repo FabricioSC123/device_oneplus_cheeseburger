0ca26e0bd065cde4e0582a5e1b481f0652f48b40
FabricioSC123 <fabriciosalaspaypal@gmail.com>
2022-03-16 23:18:01 -0500

    cheeseburger: delete cacche

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,16 +14,4 @@ cd ../../..
 else
 git clone https://github.com/FabricioSC123/vendor_oneplus_cheeseburger.git  vendor/oneplus/cheeseburger
 fi
-# hardware
-if [[ -e hardware/oneplus ]];then
-cd hardware/oneplus
-git pull
-cd ../..
-else
-git clone https://github.com/PixelExperience/hardware_oneplus.git hardware/oneplus
-fi
-# Add Ccache Fix
-export USE_CCACHE=1
-export CCACHE_EXEC=/usr/bin/ccache
-export CCACHE_DIR=/mnt/ccache
-ccache -M 50G -F 0 
+
